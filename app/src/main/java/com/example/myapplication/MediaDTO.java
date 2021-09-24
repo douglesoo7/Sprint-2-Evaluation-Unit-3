b084b4d1e2c6eb386b03b8a31811629846b3a9b4
Dougles <maddysachin786@gmail.com>
2021-09-24 15:28:01 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MediaDTO.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
+
+public class MediaDTO implements Serializable {
+
+	@SerializedName("imageUrl")
+	private String imageUrl;
+
+	public void setImageUrl(String imageUrl){
+		this.imageUrl = imageUrl;
+	}
+
+	public String getImageUrl(){
+		return imageUrl;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"MediaDTO{" + 
+			"imageUrl = '" + imageUrl + '\'' + 
+			"}";
+		}
+}
---
+++ app/src/main/java/com/example/myapplication/SneakerViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SneakerViewHolder extends RecyclerView.ViewHolder {
+    public SneakerViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
